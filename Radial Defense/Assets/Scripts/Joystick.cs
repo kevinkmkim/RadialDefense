05b53d98ff6a795e89d0001e6a91b5c11cbfd9fa
Kevin Kim <kk946@cornell.edu>
2022-11-18 17:00:29 -0500

    Refined joystick interface

--- Radial Defense/Assets/Scripts/Joystick.cs
+++ Radial Defense/Assets/Scripts/Joystick.cs
@@ -7,36 +7,57 @@ using UnityEngine.UI;
 public class Joystick : MonoBehaviour
 {
     private GameObject joystick;
+
     private Vector2 positionInput;
+
+    private bool touched;
+
     [SerializeField]
     private float radius;
 
     void Start()
     {
         joystick = transform.GetChild(0).gameObject;
-        radius = 0.5f;
+        radius = 0.3f;
+        touched = false;
     }
-    
+
     void Update()
     {
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
-            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
-            touchPosition.z = 0f;
-            joystick.transform.position = touchPosition;
-            float vectorLength = joystick.transform.localPosition.magnitude;
-            Debug.Log("Vector Length " + vectorLength);
-            Debug.Log("Radius " + radius);
-            if (vectorLength > radius)
+            Vector3 touchPosition =
+                Camera.main.ScreenToWorldPoint(touch.position);
+            Vector2 touchPosition2D =
+                new Vector2(touchPosition.x, touchPosition.y);
+
+            RaycastHit2D hit =
+                Physics2D
+                    .Raycast(touchPosition2D, Camera.main.transform.forward);
+            if (
+                hit.collider != null &&
+                hit.transform.gameObject.name == "Movement Controller"
+            )
             {
-                joystick.transform.localPosition /= vectorLength / radius;
-                // Debug.Log("Exceeded");
+                touched = true;
+            }
+
+            if (touched)
+            {
+                touchPosition.z = 0f;
+                joystick.transform.position = touchPosition;
+                float vectorLength = joystick.transform.localPosition.magnitude;
+                if (vectorLength > radius)
+                {
+                    joystick.transform.localPosition /= vectorLength / radius;
+                }
             }
         }
         else
         {
-            joystick.transform.localPosition = new Vector3(0,0,0);
+            joystick.transform.localPosition = new Vector3(0, 0, 0);
+            touched = false;
         }
     }
 }
